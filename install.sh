0f00f22f2e33072ad460afb9fc626f7325fd1665
Roman A. Nosov <roman@nosoff.info>
2025-09-27 11:33:01 +0600

    beautify script

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # Script to install usb-modeswitch on macOS
 
-set -e
-
 # Check for Homebrew and install if not present
 if ! command -v brew >/dev/null 2>&1; then \
     echo "Homebrew not found. Installing..."; \
@@ -18,13 +16,15 @@ cd ~;
 # Link can be changed to the latest version from https://www.draisberghof.de/usb_modeswitch/
 curl -LO https://www.draisberghof.de/usb_modeswitch/usb-modeswitch-2.6.0.tar.bz2;
 tar -xjf usb-modeswitch-2.6.0.tar.bz2;
-cd usb-modeswitch-2.6.0;
-make install;
 
 # Clean up
-rm -rf ../usb-modeswitch-2.6.0.tar.bz2;
+rm -rf ./usb-modeswitch-2.6.0.tar.bz2;
 
+cd usb-modeswitch-2.6.0;
 
-echo "usb-modeswitch installation completed.";
+echo "All dependencies installed.";
+
+make install 1> install.log 2>&1 || { echo "Check install.log for details of building."; }
+echo "usb-modeswitch building complete. Check your home directory for the usb-modeswitch-2.6.0 folder.";
 
 echo "You can add new configuration manually or find existing ones at https://www.draisberghof.de/usb_modeswitch/usb-modeswitch-data-20191128.tar.bz2 .";
