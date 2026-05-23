8e7cdffc040bec6d906e6e94262e8871bd89e668
Roman A. Nosov <roman@nosoff.info>
2026-05-23 21:27:20 +0600

    2.6.2 + github build workflow

--- build_with_static_linking/build_for_current_platform.sh
+++ build_with_static_linking/build_for_current_platform.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 
+modeswitchversion=$(cat ../modeswitchversion.txt)
+
+# Script to install usb-modeswitch on macOS
+
+# Check for Homebrew and install if not present
+if ! command -v brew >/dev/null 2>&1; then
+    echo "Homebrew not found. Installing...";
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+fi; 
+
 brew install libusb pkg-config; 
 
-if ! [ -d "../usb-modeswitch-2.6.0" ]; then
-    echo "usb-modeswitch-2.6.0 directory does not exist. Fetching and extracting usb-modeswitch-2.6.0"
+if ! [ -d "../usb-modeswitch-${modeswitchversion}" ]; then
+    echo "usb-modeswitch-${modeswitchversion} directory does not exist. Fetching and extracting usb-modeswitch-${modeswitchversion}"
     cd ..
-    curl -LO https://www.draisberghof.de/usb_modeswitch/usb-modeswitch-2.6.0.tar.bz2
-    tar -xjf usb-modeswitch-2.6.0.tar.bz2
-    rm -rf ./usb-modeswitch-2.6.0.tar.bz2;
+    curl -LO "https://www.draisberghof.de/usb_modeswitch/usb-modeswitch-${modeswitchversion}.tar.bz2"
+    tar -xjf "usb-modeswitch-${modeswitchversion}.tar.bz2"
+    rm -rf ./"usb-modeswitch-${modeswitchversion}.tar.bz2";
     cd ./build_with_static_linking
 else
-    echo "usb-modeswitch-2.6.0 directory already exists. Skipping fetch and extract."
+    echo "usb-modeswitch-${modeswitchversion} directory already exists. Skipping fetch and extract."
 fi
 
-cp ./Makefile ../usb-modeswitch-2.6.0/Makefile
-cd ../usb-modeswitch-2.6.0
+cp ./Makefile ../"usb-modeswitch-${modeswitchversion}"/Makefile
+cd ../"usb-modeswitch-${modeswitchversion}"
 
 make all-static 1> install.log 2>&1 || { echo "Check install_arm.log for details of building."; exit 1; }
-echo "usb-modeswitch building for current platform complete. Check your usb-modeswitch-2.6.0 folder."
+echo "usb-modeswitch building for current platform complete. Check your usb-modeswitch-${modeswitchversion} folder."
+
+cp ./usb_modeswitch-x64 ../
